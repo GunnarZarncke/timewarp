7df36a4dc950a6eae5405856b97c39c6e19a40c9
Gunnar Zarncke <gunnar.zarncke@gmx.de>
2019-02-17 12:49:09 +0100

    added vector rotion needed for future tests

--- src/main/kotlin/de/zarncke/timewarp/math/Vector.kt
+++ src/main/kotlin/de/zarncke/timewarp/math/Vector.kt
@@ -1,6 +1,6 @@
 package de.zarncke.timewarp.math
 
-import kotlin.math.sqrt
+import kotlin.math.*
 
 
 val V3_0 = Vector3(0.0, 0.0, 0.0)
@@ -46,3 +46,60 @@ data class Vector4(val t: Double, val x: Double, val y: Double, val z: Double) :
 }
 
 val vector4comparator = compareBy<Vector4>(Vector4::t, Vector4::x, Vector4::y, Vector4::z)
+
+/**
+ * https://en.wikipedia.org/wiki/Rotation_formalisms_in_three_dimensions
+ */
+fun eulerAxisToRotMatrix(eulerAxis: Vector3, phi: Double) = eulerAxis.unit().run {
+    val sinphi = sin(phi)
+    val cosphi = cos(phi)
+    val omcosPhi = 1 - cosphi
+    Matrix33(
+        omcosPhi * x * x + cosphi, omcosPhi * x * y - z * sinphi, omcosPhi * x * z + y * sinphi,  //
+        omcosPhi * y * x + z * sinphi, omcosPhi * y * y + cosphi, omcosPhi * y * z - x * sinphi, //
+        omcosPhi * z * x - y * sinphi, omcosPhi * z * y + x * sinphi, omcosPhi * z * z + cosphi
+    )
+}
+
+operator fun Vector3.times(matrix: Matrix33): Vector3 = this.run {
+    matrix.run {
+        Vector3(
+            x * a11 + y * a21 + z * a31,
+            x * a12 + y * a22 + z * a32,
+            x * a13 + y * a23 + z * a33
+        )
+    }
+}
+
+fun Int.deg2Rad() = this * PI / 180
+fun Double.rad2Deg() = this * 180 / PI
+
+/**
+ * Rotates a vector around the given vector in mathematical positive direction (right hand rule).
+ */
+fun Vector3.rotate(around: Vector3, angleRad: Double) = this.times(eulerAxisToRotMatrix(around, angleRad))
+
+// PERFORMANCE: these could be optimized:
+fun Vector3.rotateX(angleRad: Double) = this.times(eulerAxisToRotMatrix(EX, angleRad))
+
+fun Vector3.rotateY(angleRad: Double) = this.times(eulerAxisToRotMatrix(EY, angleRad))
+fun Vector3.rotateZ(angleRad: Double) = this.times(eulerAxisToRotMatrix(EZ, angleRad))
+
+
+data class Matrix33(
+    val a11: Double, val a12: Double, val a13: Double,
+    val a21: Double, val a22: Double, val a23: Double,
+    val a31: Double, val a32: Double, val a33: Double
+) {
+    fun toV3() = Triple(Vector3(a11, a12, a13), Vector3(a21, a22, a23), Vector3(a31, a32, a33))
+    fun toArray() = arrayOf(arrayOf(a11, a12, a13), arrayOf(a21, a22, a23), arrayOf(a31, a32, a33))
+    /**
+     * {\displaystyle {\begin{aligned}\theta &=\arccos {\frac {A_{11}+A_{22}+A_{33}-1}{2}}\\e_{1}&={\frac {A_{32}-A_{23}}{2\sin \theta }}\\e_{2}&={\frac {A_{13}-A_{31}}{2\sin \theta }}\\e_{3}&={\frac {A_{21}-A_{12}}{2\sin \theta }}\end{aligned}}}
+     * https://en.wikipedia.org/wiki/Rotation_formalisms_in_three_dimensions
+     */
+    fun toEulerAxisAndAngle() = run {
+        val phi = acos((a11 + a22 + a33) / 2)
+        val tsp = 2 * sin(phi)
+        Vector3((a32 - a23) / tsp, (a13 - a32) / tsp, (a21 - a12) / tsp) to phi
+    }
+}
---
+++ src/test/kotlin/de/zarncke/timewarp/math/VectorTest.kt
@@ -0,0 +1,45 @@
+package de.zarncke.timewarp.math
+
+import de.zarncke.timewarp.assertEqualsV
+import de.zarncke.timewarp.math.*
+import koma.mat
+import koma.matrix.Matrix
+import org.junit.Assert
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class VectorTest {
+    @Test
+    fun testVectorRotation() {
+        assertEqualsV(EX, EY.rotate(EZ, 90.deg2Rad()))
+    }
+
+    @Test
+    fun testCompareKoma() {
+
+        val tvec = EX + EY * 2.0 + EZ * 3.0
+        val tmat = eulerAxisToRotMatrix(EZ, 90.deg2Rad())
+        val tres = tvec * tmat
+
+
+        val kvec = mat[tvec.x, tvec.y, tvec.z]
+        val kmat = Matrix<Double>(3, 3) { r, c -> tmat.toArray()[r][c] }
+        val kres = kvec * kmat
+
+        assertEqualsV(tres, Vector3(kres.getDouble(0,0), kres.getDouble(0,1), kres.getDouble(0,2)))
+
+        val ktres = mat[tres.x, tres.y, tres.z]
+        assertEquals(kres, ktres)
+    }
+
+    @Test
+    fun testVectorRotationComplex() {
+        val someVector = EX + EY * 2.0 + EZ * 3.0
+        val someOtherVector = EX * 2.0 + EY * 3.0 + EZ * 4.0
+
+        var fiveTimesRotated = someVector
+        for (i in 1..5) fiveTimesRotated = fiveTimesRotated.rotate(someOtherVector, (360 / 5).deg2Rad())
+        assertEqualsV(someVector, fiveTimesRotated)
+    }
+
+}
